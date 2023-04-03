b16d1dbd3cdc19d3cd58737b8774db61bc835512
nicolas-castaneda <nicolas.castaneda@utec.edu.pe>
2023-04-03 01:03:10 -0500

    p2 completo

--- p2.h
+++ p2.h
@@ -33,6 +33,7 @@ std::ostream & operator<<(std::ostream & stream, Alumno & record){
     stream.write(record.carrera, 15);
     stream.write(reinterpret_cast<char *>(&record.ciclo), sizeof(record.ciclo));
     stream.write(reinterpret_cast<char *>(&record.mensualidad), sizeof(record.mensualidad));
+
     stream << "\r\n";
     stream<<std::flush;
     return stream;
@@ -47,32 +48,134 @@ public:
         infile.open(this->filename, std::ios::in | std::ios::binary);
         std::vector<Alumno> result;
         if (infile.is_open()) {
-            Alumno tmp;
-            while(infile.read((char *) &tmp, sizeof(tmp))){
-                result.push_back(tmp);
+            int header;
+            infile.read((char *) &header, sizeof(header));
+
+            Alumno record;
+            while(infile.read((char *) &record, sizeof(record))){
+
+                bool eliminado;
+                infile.read((char *) &eliminado, sizeof(eliminado));
+
+                if (!eliminado) {
+                    result.push_back(record);
+                }
+
+                int next_del;
+                infile.read((char *) &next_del, sizeof(next_del));
             }
             infile.close();
         }
         return result;
     };
     void add(const Alumno& record){
-        std::ofstream outfile;
-        outfile.open(this->filename, std::ios::app | std::ios::binary);
-        outfile.write((char*) &record, sizeof(record));
-        outfile.close();
+        std::fstream file;
+        file.open(this->filename, std::ios::in | std::ios::out | std::ios::binary);
+        int header;
+
+        if(!file) {
+            file.close();
+
+            std::ofstream outfile;
+            outfile.open(this->filename, std::ios::app | std::ios::binary);
+            header = -1;
+            outfile.write((char*) &header, sizeof(header));
+            outfile.close();
+
+            file.open(this->filename, std::ios::in | std::ios::out | std::ios::binary);
+        } else {
+            file.read((char *) &header, sizeof(header));
+        }
+
+        bool eliminado = false;
+        int next_del = 0;
+
+        if (header == -1){
+            file.close();
+            std::ofstream outfile;
+            outfile.open(this->filename, std::ios::app | std::ios::binary);
+            outfile.write((char*) &record, sizeof(record));
+
+            outfile.write((char*) &eliminado, sizeof(eliminado));
+            outfile.write((char*) &next_del, sizeof(next_del));
+
+            outfile.close();
+            return;
+        } else {
+            file.seekg(sizeof(header) + (header)*(sizeof(record) + sizeof(eliminado) + sizeof(next_del)) + sizeof(record) + sizeof(eliminado), std::ios::beg);
+
+            file.read((char *) &next_del, sizeof(next_del));
+
+            file.seekg(0, std::ios::beg);
+            file.write((char *) &next_del, sizeof(next_del));
+
+            file.seekg(sizeof(header) + (header)*(sizeof(record) + sizeof(eliminado) + sizeof(next_del)), std::ios::beg);
+            next_del = 0;
+
+            file.write((char *) &record, sizeof(record));
+            file.write((char *) &eliminado, sizeof(eliminado));
+            file.write((char *) &next_del, sizeof(next_del));
+        }
+
     };
 
     Alumno readRecord(int pos){
         std::ifstream infile;
         infile.open(this->filename, std::ios::binary);
-        Alumno tmp;
-        infile.seekg(pos * sizeof(tmp), std::ios::beg);
-        infile.read((char *) &tmp, sizeof(tmp));
+
+        Alumno record;
+        bool eliminado;
+
+        infile.seekg(sizeof(int) + pos * (sizeof(record) + sizeof(eliminado) + sizeof(int)) , std::ios::beg);
+        infile.read((char *) &record, sizeof(record));
+
+        infile.read((char *) &eliminado, sizeof(eliminado));
+
         infile.close();
-        return tmp;
+        if (!eliminado) {
+            return record;
+        } else {
+            Alumno empty;
+            return empty;
+        }
     };
 
     bool del(int pos){
+        std::fstream file;
+        file.open(this->filename, std::ios::in | std::ios::out | std::ios::binary);
+
+        int header;
+        file.read((char *) &header, sizeof(header));
+        file.seekg(0, std::ios::beg);
+        file.write((char *) &pos, sizeof(pos));
+
+        Alumno record;
+        bool eliminado;
+        int next_del;
+
+        file.seekg(sizeof(header) + (pos)*(sizeof(record) + sizeof(eliminado) + sizeof(next_del)) + sizeof(record), std::ios::beg);
+
+        file.read((char *) &eliminado, sizeof(eliminado));
+
+        file.read((char *) &next_del, sizeof(next_del));
+
+        if (header == -1) {
+            eliminado = true;
+            next_del = -1;
+        } else {
+            if (eliminado) {
+                file.close();
+                return false;
+            }
+            eliminado = true;
+            next_del = header;
+        }
+
+        file.seekg(sizeof(header) + (pos)*(sizeof(record) + sizeof(eliminado) + sizeof(next_del)) + sizeof(record), std::ios::beg);
+        file.write((char *) &eliminado, sizeof(eliminado));
+        file.write((char *) &next_del, sizeof(next_del));
+
+        file.close();
         return true;
     };
 };
@@ -86,8 +189,8 @@ void assignString2CharArray(std::string temp, char buffer[], int size){
 void test2(){
     FixedRecord fr("datos_alumnos.bin");
     Alumno a = Alumno();
-    assignString2CharArray("0001", a.codigo, sizeof(a.codigo));
-    assignString2CharArray("juaquin3    ", a.nombre, sizeof(a.nombre));
+    assignString2CharArray("0010", a.codigo, sizeof(a.codigo));
+    assignString2CharArray("juaquin312   ", a.nombre, sizeof(a.nombre));
     assignString2CharArray("Remon00                         ", a.apellidos, sizeof(a.apellidos));
     assignString2CharArray("Computacion          ", a.carrera, sizeof(a.carrera));
 
@@ -95,6 +198,16 @@ void test2(){
     a.mensualidad = 0.99;
 
     fr.add(a);
+
+    assignString2CharArray("0011", a.codigo, sizeof(a.codigo));
+    fr.add(a);
+
+    assignString2CharArray("0012", a.codigo, sizeof(a.codigo));
+    fr.add(a);
+
+    assignString2CharArray("0013", a.codigo, sizeof(a.codigo));
+    fr.add(a);
+
     auto t = fr.load();
     std::cout << t.size() << std::endl;
     for (auto & e: t){
@@ -106,7 +219,7 @@ void test2(){
         std::cout << e.mensualidad << std::endl;
     }
     std::cout << "---------------------" << std::endl;
-    Alumno b = fr.readRecord(10);
+    Alumno b = fr.readRecord(0);
 
     std::cout << b.codigo << " ";
     std::cout << b.nombre << " ";
@@ -114,4 +227,20 @@ void test2(){
     std::cout << b.carrera << " ";
     std::cout << b.ciclo << " ";
     std::cout << b.mensualidad << std::endl;
+
+    std::cout << "---------------------" << std::endl;
+    /* fr.del(1);
+    fr.del(2);
+    fr.del(3);
+    t = fr.load();
+    for (auto & e: t){
+        std::cout << e.codigo << " ";
+        std::cout << e.nombre << " ";
+        std::cout << e.apellidos << " ";
+        std::cout << e.carrera << " ";
+        std::cout << e.ciclo << " ";
+        std::cout << e.mensualidad << std::endl;
+    } */
+
+
 }
